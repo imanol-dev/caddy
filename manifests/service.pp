4fd4ba9188132cdb1a088b73d3b59bfb8fc72aa6
System Administrator <root@MacBook-Pro-de-Imanol.local>
2021-03-28 03:33:22 +0200

    FIX service variables

--- manifests/service.pp
+++ manifests/service.pp
@@ -7,15 +7,15 @@
 class caddy::service {
 
   if $caddy::package_ensure == 'absent' {
-    $_ensure_service = 'stopped'
-    $_enable_service = false
+    $ensure_service_param = 'stopped'
+    $enable_service_param = false
   } else {
-    $_ensure_service = $caddy::ensure_service
-    $_enable_service = $caddy::enable_service
+    $ensure_service_param = $caddy::ensure_service
+    $enable_service_param = $caddy::enable_service
   }
 
   service { 'caddy':
-    ensure => $_ensure_service,
-    enable => $_enable_service
+    ensure => $ensure_service_param,
+    enable => $enable_service_param
   }
 }
